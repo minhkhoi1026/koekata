14ca7fcd6dea4e6e05a1dcc9a6409fd2340caf0f
Zero-nnkn <nnkhoinguyen9@gmail.com>
2021-12-16 15:40:31 +0700

    up statistic

---
+++ src/koekata/app/src/main/java/com/example/koekata/ui/main/Statistic/StatisticFragment.java
@@ -0,0 +1,8 @@
+package com.example.koekata.ui.main.Statistic;
+
+import dagger.android.support.DaggerFragment;
+
+public class StatisticFragment extends DaggerFragment {
+
+
+}
---
+++ src/koekata/app/src/main/java/com/example/koekata/ui/main/Statistic/StatisticViewModel.java
@@ -0,0 +1,7 @@
+package com.example.koekata.ui.main.Statistic;
+
+import androidx.lifecycle.ViewModel;
+
+public class StatisticViewModel extends ViewModel {
+
+}
